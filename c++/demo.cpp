45aedafc97f30f12e1486dc08bb77c30e1a6eef2
Fahad <fahad.abdullah1280@gmail.com>
2024-05-01 16:43:02 +0600

    Create triangle

--- c++/demo.cpp
+++ c++/demo.cpp
@@ -12,15 +12,16 @@ int main() {
    int num;
    cin >> num;
    
-   for ( int i=num; i >= 1; i--)
-   {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-        
-   }
+for (int i = 0; i < num; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+    
+}
+
    
 
    return 0;
