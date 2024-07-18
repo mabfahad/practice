77afe1668e11e7b31bff30231860495d641c5482
Fahad <fahad.abdullah1280@gmail.com>
2024-07-18 16:18:34 +0600

    JS object

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
-alert('test');
+let person = {
+    name:'Fahad',
+    age: 33,
+    isMarraid: true,
+    printDetails(name,age,isMarraid) {
+        return `this is ${name}. He is ${age} years old and married ${isMarraid}`;
+    }
+}
+
+console.log(person.printDetails('Fahad',33,false));
