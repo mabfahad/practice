2d278315bea9befc809b1cb18d370b373b94caea
Fahad <fahad.abdullah1280@gmail.com>
2024-07-30 10:20:31 +0600

    Add opacity if scrollY

--- single-page-design/js/main.js
+++ single-page-design/js/main.js
@@ -1,24 +1,35 @@
-let map;
+let map
 
 async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+  const { Map } = await google.maps.importLibrary('maps')
 
-  map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById('map'), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
-  });
+  })
 }
 
-initMap();
+initMap()
+
+//Sticky menu background
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.8;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+})
 
 //Smooth Scrolling
-jQuery(function(){
-   jQuery("#navbar a, .btn").on("click",function(event){
-    event.preventDefault();
-    const hash = this.hash;
-    jQuery('html,body').animate({
-      scrollTop: jQuery(hash).offset().top-100
-    },800);
-   });
-
-});
+jQuery(function () {
+  jQuery('#navbar a, .btn').on('click', function (event) {
+    event.preventDefault()
+    const hash = this.hash
+    jQuery('html,body').animate(
+      {
+        scrollTop: jQuery(hash).offset().top - 100,
+      },
+      800
+    )
+  })
+})
