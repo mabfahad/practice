4504a1ef532cd83bbb187a324fca1c54509b86cc
Fahad Abdullah <fahad.abdullah1280@gmail.com>
2025-10-23 12:54:47 +0600

    while loop

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,15 @@
 const a = 12
 const b = 12
 let c = a + b
-c = 34
-console.log(c)
+
+let arr = [1,2,3,4,5]
+
+for (let i=0;i<arr.length;i++) {
+    console.log(arr[i])
+}
+
+let i = 0
+while (i<arr.length) {
+    console.log("dfdf"+arr[i])
+    i++
+}
